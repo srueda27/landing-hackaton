a33105e4607ca4eedbadad430b9911bbd39719bc
Santiago Rueda Ortiz <srueda27@gmail.com>
2020-07-12 09:48:04 -0500

    add 4k fontsize

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+$('#inicio-span').click(function() {
+    var elmnt = document.getElementById("CaliHacktech");
+    elmnt.scrollIntoView(true);
+});
+
+$('#inscripcion-span').click(function() {
+    var elmnt = document.getElementById("Inscripcion");
+    elmnt.scrollIntoView(false);
+});
+
+$('#patrocinadores-span').click(function() {
+    var elmnt = document.getElementById("Patrocinadores");
+    elmnt.scrollIntoView(true);
+});
+
+$('#test').click(function() {
+    var elmnt = document.getElementById("Inscripcion");
+    elmnt.scrollIntoView(false);
+});
