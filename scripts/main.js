5e9c59bb1bf51334b741b3d54df3d501dc72a5a9
Santiago Rueda Ortiz <srueda27@gmail.com>
2020-07-15 06:23:45 -0500

    finish landing. Missing Términos y condiciones content

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,40 @@
 $('#inicio-span').click(function() {
-    var elmnt = document.getElementById("CaliHacktech");
+    var elmnt = document.getElementById('CaliHacktech');
     elmnt.scrollIntoView(true);
 });
 
 $('#inscripcion-span').click(function() {
-    var elmnt = document.getElementById("Inscripcion");
+    var elmnt = document.getElementById('Inscripcion');
     elmnt.scrollIntoView(false);
 });
 
 $('#patrocinadores-span').click(function() {
-    var elmnt = document.getElementById("Patrocinadores");
+    var elmnt = document.getElementById('Patrocinadores');
     elmnt.scrollIntoView(true);
 });
 
 $('#retos-span').click(function() {
-    var elmnt = document.getElementById("Retos");
+    var elmnt = document.getElementById('Retos');
     elmnt.scrollIntoView(true);
+});
+
+$('#jurados-span').click(function() {
+    var elmnt = document.getElementById('Jurados');
+    $("#Mentores").addClass('inactive');
+    $("#Jurados").removeClass('inactive');
+    elmnt.scrollIntoView(true);
+});
+
+$('#mentores-span').click(function() {
+    var elmnt = document.getElementById('Mentores');
+    $("#Mentores").removeClass('inactive');
+    $("#Jurados").addClass('inactive');
+    elmnt.scrollIntoView(true);
+});
+
+$(document).ready(function(){
+    $('.toggle-btn').click(function() {
+        $('#Mentores').toggleClass('inactive');
+        $('#Jurados').toggleClass('inactive');
+    });
 });
